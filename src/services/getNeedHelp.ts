58225fa67d83362823ffb00a8ec2f76d443a0b35
r09-Andrey <r09-andrey@monowork.pro>
2024-06-06 16:18:57 +0400

    feat: test subscription

--- src/services/getNeedHelp.ts
+++ src/services/getNeedHelp.ts
@@ -3,11 +3,9 @@ import axios from 'axios';
 const { VITE_APP_API_URL } = import.meta.env;
 
 export const getNeedHelp = async (userId: number, bot_username: string) => {
-  const res = await axios.post(`${VITE_APP_API_URL}/need_help/${userId}`, {
-    data: {
-      bot_username,
-    },
-  });
+  const res = await axios.post(
+    `${VITE_APP_API_URL}/need_help/${userId}?bot_username=${bot_username}`,
+  );
   const data = res.data;
 
   return data;
