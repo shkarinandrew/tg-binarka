01ad0407be9f68241fd75b00273a2cf2f8610384
r09-Andrey <r09-andrey@monowork.pro>
2024-06-06 13:17:58 +0400

    fix: getNeedHelp переделан на POST

--- src/services/getNeedHelp.ts
+++ src/services/getNeedHelp.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-export const getNeedHelp = async (userId: string, botUsername: string) => {
-  const res = await axios.get(
-    `https://notwebnotapp.click/api/need_help/${userId}/${botUsername}`,
-  );
+export const getNeedHelp = async (userId: string, bot_username: string) => {
+  const res = await axios.post(`https://notwebnotapp.click/api/need_help/${userId}`, {
+    data: {
+      bot_username,
+    },
+  });
   const data = res.data;
 
   return data;
