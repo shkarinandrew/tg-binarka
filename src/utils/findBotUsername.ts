d527202c00e9efd79db31201b7ec951bf405ab69
r09-Andrey <r09-andrey@monowork.pro>
2024-06-06 15:54:29 +0400

    feat: test subscription

--- src/utils/findBotUsername.ts
+++ src/utils/findBotUsername.ts
@@ -1,7 +1,7 @@
 export const findBotUsername = () => {
-  // const urlParams = new URLSearchParams(window.location.search);
-  // const botUsername = urlParams.get('bot_username');
-  const botUsername = 'binarkagogogo_bot';
+  const urlParams = new URLSearchParams(window.location.search);
+  const botUsername = urlParams.get('bot_username');
+  // const botUsername = 'binarkagogogo_bot';
 
   return botUsername;
 };
