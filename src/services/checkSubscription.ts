bde09385cfdea2ab790384b687717607b3ee2463
r09-Andrey <r09-andrey@monowork.pro>
2024-07-09 02:01:59 +0400

    fix: подписка на канал

--- src/services/checkSubscription.ts
+++ src/services/checkSubscription.ts
@@ -3,9 +3,11 @@ import axios from 'axios';
 const { VITE_APP_API_URL } = import.meta.env;
 
 export const checkSubscription = async (userId: number, botUsername: string) => {
-  const res = await axios.post<boolean>(`${VITE_APP_API_URL}/check-subscription`, {
-    userId,
-    botUsername,
+  const res = await axios.get<boolean>(`${VITE_APP_API_URL}/check-subscription`, {
+    params: {
+      userId,
+      botUsername,
+    },
   });
   const data = res.data;
 
